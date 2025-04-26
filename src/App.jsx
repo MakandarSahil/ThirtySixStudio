a3f2445907b6b51f2f98c7a1aea981a4a9593e4a
MakandarSahil <sahilmakandar15@gmail.com>
2025-04-26 16:27:51 +0530

    canvas loaded on their positions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Canvas from "./components/Canvas";
+import data from "./canvas/data";
 
 function App() {
   return (
     <>
-      <div className="w-full min-h-screen bg-black text-white">
-        <Canvas startIndex={0} />
-        <Canvas startIndex={150} />
-        <Canvas startIndex={300} />
+      <div className="w-full relative min-h-screen">
+        {data[0].map((canvasDetails, index) => (
+          <Canvas key={index} details={canvasDetails} />
+        ))}
+      </div>
+      <div className="w-full relative min-h-screen">
+        {data[1].map((canvasDetails, index) => (
+          <Canvas key={index} details={canvasDetails} />
+        ))}
+      </div>
+      <div className="w-full relative min-h-screen">
+        {data[2].map((canvasDetails, index) => (
+          <Canvas key={index} details={canvasDetails} />
+        ))}
       </div>
     </>
   );
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
-import canvasImage from "../assets/canvasimages";
+import canvasImage from "../canvas/canvasimages";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
-function Canvas({ startIndex }) {
+function Canvas({ details }) {
+  const { startIndex, numImages, duration, size, top, left, zIndex } = details;
   const [index, setIndex] = useState({ value: startIndex });
   const canvasRef = useRef(null);
 
   useGSAP(() => {
     gsap.to(index, {
-      value: startIndex + 149,
-      duration: 3,
+      value: startIndex + numImages - 1,
+      duration: duration,
       repeat: -1,
       ease: "linear",
       onUpdate: () => {
@@ -20,20 +21,32 @@ function Canvas({ startIndex }) {
   });
 
   useEffect(() => {
+    const scale = window.devicePixelRatio;
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     const img = new Image();
     img.src = canvasImage[index.value];
     img.onload = () => {
-      canvas.width = img.width;
-      canvas.height = img.height;
-      ctx.drawImage(img, 0, 0);
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
+
+      ctx.scale(scale, scale);
+      ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
     };
   }, [index]);
 
   return (
     <canvas
-      className="w-[14rem] h-[14rem]"
+      className="absolute"
+      style={{
+        width: `${size * 1.4}px`,
+        height: `${size * 1.4}px`,
+        top: `${top}%`,
+        left: `${left}%`,
+        zIndex: `${zIndex}`,
+      }}
       ref={canvasRef}
       id="canvas"
     ></canvas>
